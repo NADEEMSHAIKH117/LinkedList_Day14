643a8c5c268a9935655ff767264fe053f55cad9e
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-05 20:55:10 +0530

    added

--- src/Bridgelabz/LinkedListMain.java
+++ src/Bridgelabz/LinkedListMain.java
@@ -1,22 +1,19 @@
 package Bridgelabz;
 
+import java.util.Scanner;
+
 public class LinkedListMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to LinkedList Code !! ");
-		Node firstNode = new Node(56);
-		Node secondNode = new Node(30);
-		Node thirdNode = new Node(70);
-		
-		Node head = firstNode;
-		firstNode.next = secondNode;
-		secondNode.next = thirdNode;
-		Node temp = head;
-		while (temp != null) {
-			System.out.println(temp.data );
-			temp = temp.next;
-		}
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Press '1' to add the data at the start");
+        switch (scanner.nextInt()) {
+        	case 1:{
+        		Operations.addDataAtStart();
+        	}
+        }
+        
 	}
-
+	
 }
--- src/Bridgelabz/Node.java
+++ src/Bridgelabz/Node.java
@@ -3,6 +3,8 @@ package Bridgelabz;
 public class Node {
 	public int data;
 	public Node next;
+	
+	
 	public Node(int data) {
 		this.data=data;
 		this.next=null;
